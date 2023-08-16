aacf19df0bec68c404b0234f49a0ba846d0170a4
ozvshhh <dlgkdms4660@gmail.com>
2023-08-16 20:21:41 +0900

    8.16 알고리즘 스터디 숙제검사

--- 7795.py
+++ 7795.py
@@ -14,7 +14,7 @@ for t in range(T):
     search_idx = len(predator)-1
     target_idx = len(prey)-1
     
-    while target_idx >= 0:
+    while target_idx >= 0 and search_idx >= 0:
         #print("search:",search_idx)
         #print("target:",target_idx)
         if predator[search_idx] == prey[target_idx]:
@@ -24,10 +24,16 @@ for t in range(T):
         elif predator[search_idx] < prey[target_idx]:
             count += len(predator)-(search_idx+1)
             target_idx -= 1
-        elif search_idx < 0:
+        elif search_idx == 0: #두 인덱스 중 하나가 끝에 있을 때 +1 씩 해준다
             count += len(predator)
             target_idx -= 1
         else:
             search_idx -= 1
         
     print(count)
+    
+    
+    '''
+    1 1=(1) 3=(3) (6)7 8
+    1 3 6
+    '''
