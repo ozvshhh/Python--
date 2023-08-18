6cdd305a8a51d7ef8c290177418ad122b635a6a2
ozvshhh <dlgkdms4660@gmail.com>
2023-08-19 00:55:39 +0900

    나무자르기 드디어 성공

--- 2805.py
+++ 2805.py
@@ -1,63 +1,42 @@
-#목재절단기
+#나무 자르기
 '''
-높이 H
-그 높이에 이상은 다 절단
-낮은나무 잘리지 않음
-20 15 10 17 일때
-높이 15이면
-15 15 10 15
-7미터 집에 가져감
-상근이가 M미터의 나무를 가져가려면
-높이를 최대 몇 H미터로 설정해야 하는가
+1초
+나무의 수 N 1,000,000
+캐야할 나무의  수 2,000,000,000
+절단기 높이는 
+높이 1,000,000,000
 
-2NlogN
+절단기의 높이를 높이면, 나무의 수가 줄어들고
+절단기의 높이를 낮추면, 나무의 수가 늘어난다
 
-나무의 최댓 값
-sum(trees)
-
-나무의 최솟값
-
-0 ~ sum(trees) <- 이 안에는 있겠지
+절단기 높이를 세팅하는데 이진탐색을 이용 log 1,000,000,000
+설정된 높이를 이용하여 나무 길이 계산 1,000,000
+시간복잡도 이정도면 ㄱㅊ할듯
 
 '''
 
-'''
-20230427
-
-n 높이의 나무의 갯수를 n번index의 값으로 갖는 배열을 만들어보자
-
-나무의 높이를 입력받아 trees 배열에 저장한다.
-0 ~ max(trees)만큼 이진탐색을 진행하여
-높이를 index값으로 갖는 나무가 몇개인지 저장한다.
-가장 큰 값부터 차례대로 훑어가면서
-목표한 값이 나올 때 까지 합연산을 진행한다.
--> 시간복잡도가 너무 커서 망한다.. ㅅㅂ
-'''
-
-N, M = map(int,input().split())
+N,M = map(int,input().split())
 trees = list(map(int,input().split()))
+def cutTree(cut):
+    answer = 0
+    for tree in trees:
+        if tree-cut > 0:
+            answer += tree-cut
+    return answer
 
-def getBS(list):
-    global M
-    start = min(list)
-    end = max(list)
-    save = 0
+def BinSearch():
+    start = 0
+    end = max(trees)
     while start <= end:
-        mid = (start+end)//2
-        namu=get_woods(list,mid)
-        if M <= namu:
-            save = mid
+        #print(0,start, end)
+        mid = (start + end)//2
+        get = cutTree(mid)
+        #print(get)
+        if get >= M:
             start = mid + 1
+            #print(1,start,end)
         else:
-            print(save)
-            break
-
-def get_woods (list2,h):
-    j = 0
-    for i in range(len(list2)):
-        if list2[i]-h > 0:
-             j+= list2[i]-h
-        else:
-            pass
-    return j
-getBS(trees)
+            end = mid - 1
+            #print(2,start,end)
+    return end  
+print(BinSearch())
